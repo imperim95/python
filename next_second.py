714558e57f3273d99ebbd9f762ce81229b49b04e
David Pohosyan <davpohbab@alu.edu.gva.es>
2025-10-03 11:55:04 +0200

    ficheros

---
+++ next_second.py
@@ -0,0 +1,7 @@
+#Crea un programa que demane tres enters a l'usuari, corresponent a les hores, minuts i segons.
+#Comprova que els valors siguen vàlids. Si no ho són, cal que el programa mostre un missatge d'error.
+#En cas que siga una hora vàlida, el programa ha d'afegir 1 segon a aquesta i mostrar el resultat en el format HH:MM:SS
+
+def hora(a,b,c):
+
+if
