0675d28c01086fbfcac172cbda174ddc6735c017
David Pohosyan <davpohbab@alu.edu.gva.es>
2025-10-17 11:35:33 +0200

    f

--- loop/fibonacci.py
+++ loop/fibonacci.py
@@ -1,10 +1,12 @@
 num1=0
 num2=1
-
+intermedio=0
 para=int(input("Dime un número: "))
-
 for a in range(1,para+1):
-    print(num2)
+    print(num1)
+    intermedio=num2
     num2=num2+num1
-    num1=num2
-print(num2)
+    num1=intermedio
+    
+
+
