76a6f0d89142f60544ed60a30fd2dc9d74fa4aa2
David Pohosyan <davpohbab@alu.edu.gva.es>
2025-11-02 14:19:02 +0100

    Estructura hecha

--- loop/factorial.py
+++
@@ -1,7 +0,0 @@
-#Escriu un programa que retorne el factorial d'un nombre.
-
-numero=int(input("Dime un número: "))
-resultado=1
-for a in range(1,numero+1):
-    resultado+=resultado*a
-print(resultado)
--- loop/multiply_table.py
+++
@@ -1,7 +0,0 @@
-#Crea un programa que demane a l'usuari un enter i mostre per pantalla la seua taula de multiplicar del 1 al 9.
-
-numero=int(input("Dime un numero: "))
-
-for i in range(1,11):
-    resultado=i*numero
-    print(i," * ",numero," = ",resultado)
